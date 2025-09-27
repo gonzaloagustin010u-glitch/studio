78cfa3f0f08e759ec1be5396d2c28cb3ed183a1b
Agustin Acosta <gonzaloagustin010u@gmail.com>
2025-09-27 19:41:04 +0000

    quiero que el titulo (Monetiza Tus Creaciones Digitales) cambie a (venta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             <div className="flex flex-col justify-center space-y-4">
               <div className="space-y-2">
                 <h1 className="font-headline text-4xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none text-primary">
-                  Monetiza Tus Creaciones Digitales
+                  Ventas de páginas web para tu negocio
                 </h1>
                 <p className="max-w-[600px] text-muted-foreground md:text-xl">
                   CodeCanvas es el mercado definitivo para desarrolladores y diseñadores. Convierte tus proyectos de programación y diseños web en ganancias, y descubre código y diseños de alta calidad de una comunidad global.
